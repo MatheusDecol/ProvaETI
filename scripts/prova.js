a5d4ac6e1af64981f7051fb5a12aee7a016674b2
Matheus Decol <matheusdecol22@gmail.com.br>
2025-04-08 19:47:58 -0300

    feat(prova.js) Criado constructor da classe Personagem

--- scripts/prova.js
+++ scripts/prova.js
@@ -1,5 +1,15 @@
 class Personagem{
-   
+    constructor (id, nome, nomeAventureiro, classe, level,forca, defesa) {
+        this.id = id;
+        this.nome = nome;
+        this.level = level;
+        this.classe = classe;
+        this.nomeAventureiro = nomeAventureiro;
+        this.forca = forca;
+        this.defesa= defesa;
+        this.itens = [];
+    }
+
     
 
 }
